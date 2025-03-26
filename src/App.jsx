bbfca1c71bcdb9372324365a19e6e25557e00fba
gabfmartinez <gabfmartinez@gmail.com>
2025-03-25 22:53:41 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegistroFormulario from './components/RegistroFormulario'; // Asegúrate de tener la ruta correcta
+
+function App() {
+  return (
+    <div className="App">
+      <RegistroFormulario />
+    </div>
+  );
+}
+
+export default App;
+
+import './styles.css';
